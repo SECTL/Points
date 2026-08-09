11f52264397a2020092ea7e97e8dc18c15e7a244
ShiMingXuanSimon <1923696262@qq.com>
2026-08-09 14:23:06 +0800

    完成entity.h

---
+++ include/entity.h
@@ -0,0 +1,24 @@
+# ifndef ENTITY_H
+# define ENTITY_H
+
+# include <string>
+
+struct Student {
+    int id;
+    std::string name;
+    std::string gender;
+    long long old_score, score;
+    int old_rank, rank;
+};
+
+struct Rule {
+    std::string desc;
+    int delta;
+};
+
+struct Gift {
+    std::string desc;
+    int delta;
+};
+
+#endif // ENTITY_H
